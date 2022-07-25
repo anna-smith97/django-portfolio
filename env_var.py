08bef54d8eeed4c4c215a2d9acd5346812baf058
anna-smith97 <asmith60@tulane.edu>
2022-07-25 13:35:15 -0400

    add ignore

--- env_var.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def main():
-    os.environ['DATABASE_URL'] = 'postgres://efqgkwzznvwfoo:459634ff2a72793f32ec3e1ea693b9f5a24f4ec6a8b8b590f3e02fc24dbd5861@ec2-54-87-179-4.compute-1.amazonaws.com:5432/dfamuneg8ram38'
-
-# path = os.environ.get('PATH')
-# path += ';C:\Program Files\PostgreSQL\13\bin'
-# os.environ['PATH'] = path
